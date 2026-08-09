89695a894a0e44f9c0fa23b43666737cc033e545
Anuj Singh <ianujsingh.1801@gmail.com>
2026-08-09 09:36:23 +0530

    Time: 3 ms (79.97%), Space: 19.3 MB (57.58%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,25 +1,29 @@
 class Solution:
     def romanToInt(self, user):
+
         if user=="":
-            print("input can not be null")
             return 0
+
         user = user.upper()
         r = len(user)-1
+
         RTI = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500,  "M":1000}
+
         if user[r] in RTI:
             output = RTI[user[r]]
         else:
             return 0
+
         r-=1
         while(r>=0):
-            cur = user[r]
-            pre = user[r+1]
-            if cur not in RTI or pre not in RTI:
-                    return 0
+
+            if user[r] not in RTI:
+                return 0
             else:
-                if RTI[pre]>RTI[cur]:
-                    output += (-RTI[cur])
+                if RTI[user[r+1]]>RTI[user[r]]:
+                    output += (-RTI[user[r]])
                 else:
-                    output += (RTI[cur])
+                    output += (RTI[user[r]])
             r-=1
+
         return output
