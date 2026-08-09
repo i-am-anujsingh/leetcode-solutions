921be03eb68ad52bf4795e809502134a3b2250b9
Anuj Singh <ianujsingh.1801@gmail.com>
2026-08-09 09:29:25 +0530

    Time: 5 ms (51.47%), Space: 19.3 MB (19.35%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -3,28 +3,23 @@ class Solution:
         if user=="":
             print("input can not be null")
             return 0
-        user = " ".join(user).upper().split(" ")
+        user = user.upper()
         r = len(user)-1
         RTI = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500,  "M":1000}
-        if user[r] in RTI.keys():
-            int = RTI[user[r]]
+        if user[r] in RTI:
+            output = RTI[user[r]]
         else:
             return 0
         r-=1
-        flag = False
         while(r>=0):
             cur = user[r]
-            pre = user[r+1]   
-            if pre != cur:
-                if cur not in RTI.keys() or pre not in RTI.keys():
+            pre = user[r+1]
+            if cur not in RTI or pre not in RTI:
                     return 0
-                if RTI[pre]>RTI[cur]:
-                    int = int + (-RTI[cur])
-                else:
-                    int = int + (RTI[cur])
             else:
-                if cur not in RTI.keys() or pre not in RTI.keys():
-                    return 0
-                int = int + (RTI[cur])
+                if RTI[pre]>RTI[cur]:
+                    output += (-RTI[cur])
+                else:
+                    output += (RTI[cur])
             r-=1
-        return int
+        return output
